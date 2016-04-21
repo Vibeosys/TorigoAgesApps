373dffe0064a1d6fa9b14745869bf4a6da715a49
AkshayVGadhave <akshay@vibeosys.com>
2016-04-21 20:42:59 +0530

    Tracking App
    Tracking App

---
+++ VehicleTrack/app/src/main/java/com/vibeosys/vehicletrack/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.vibeosys.vehicletrack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void callMain(View v) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+}
---
+++ VehicleTrack/app/src/main/java/com/vibeosys/vehicletrack/MyProfile.java
@@ -0,0 +1,14 @@
+package com.vibeosys.vehicletrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+        setTitle("My Details");
+    }
+}
