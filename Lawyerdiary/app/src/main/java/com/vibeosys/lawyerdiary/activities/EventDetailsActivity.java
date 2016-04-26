79c4bc40d2625479a38b59ea111e6dc096a4ca9e
AkshayVGadhave <akshay@vibeosys.com>
2016-04-26 13:33:02 +0530

    Lawyer Diary
    Lawyer Diary

---
+++ Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/activities/EventDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.vibeosys.lawyerdiary.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vibeosys.lawyerdiary.R;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+}
---
+++ Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/activities/SheduleActivity.java
@@ -0,0 +1,24 @@
+package com.vibeosys.lawyerdiary.activities;
+
+import android.graphics.RectF;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+import com.vibeosys.lawyerdiary.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SheduleActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shedule);
+
+    }
+}
