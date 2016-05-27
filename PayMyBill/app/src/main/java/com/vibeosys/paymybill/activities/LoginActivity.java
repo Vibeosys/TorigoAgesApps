622a054ae29c9e213ab33292d4806ada2fe4a152
ShrinivasSH <shrinivas@vibeosys.com>
2016-05-27 16:16:04 +0530

    login activity changes UI
    login activity changes UI

--- PayMyBill/app/src/main/java/com/vibeosys/paymybill/activities/LoginActivity.java
+++ PayMyBill/app/src/main/java/com/vibeosys/paymybill/activities/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.vibeosys.paymybill.activities;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import com.vibeosys.paymybill.R;
@@ -22,17 +24,18 @@ public class LoginActivity extends BaseActivity {
         mRegisterUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registerUser = new Intent(getApplicationContext(), UserRegisterActivity.class);
+               Intent registerUser = new Intent(getApplicationContext(), UserRegisterActivity.class);
                 startActivity(registerUser);
+
             }
         });
-        mSignIn = (Button) findViewById(R.id.sign_in_user_btn);
+        /*mSignIn = (Button) findViewById(R.id.sign_in_user_btn);
         mSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signInIntent = new Intent(getApplicationContext(), SignInActivity.class);
                 startActivity(signInIntent);
             }
-        });
+        });*/
     }
 }
