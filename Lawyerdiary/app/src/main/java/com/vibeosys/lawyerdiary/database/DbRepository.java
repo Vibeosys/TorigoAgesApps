7b61ce2424762463d3f4a18fb38ec6c14ba7171d
AkshayVGadhave <akshay@vibeosys.com>
2016-09-09 19:04:19 +0530

    Database With updated Contract Class
    Database With updated Contract Class

---
+++ Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/database/DbRepository.java
@@ -0,0 +1,99 @@
+package com.vibeosys.lawyerdiary.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.vibeosys.lawyerdiary.database.LawyerContract;
+
+import android.util.Log;
+
+/**
+ * Created by akshay on 09-09-2016.
+ */
+public class DbRepository extends SQLiteOpenHelper {
+
+    public final static String DATABASE_NAME = "LawyerDiary";
+    public final static int DATABASE_VERSION = 1;
+
+    private static final String TAG = DbRepository.class.getSimpleName();
+    private String CREATE_CLIENT_TABLE = "CREATE TABLE " + LawyerContract.Client.TABLE_NAME + "(" +
+            LawyerContract.Client._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            LawyerContract.Client.NAME + " TEXT NOT NULL," +
+            LawyerContract.Client.EMAIL + " TEXT NOT NULL UNIQUE," +
+            LawyerContract.Client.PH_NUMBER + " TEXT NOT NULL UNIQUE," +
+            LawyerContract.Client.ADDRESS + " TEXT);";
+
+    private String CREATE_CASE_TABLE = "CREATE TABLE " + LawyerContract.Case.TABLE_NAME + "(" +
+            LawyerContract.Case._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            LawyerContract.Case.CASE_NAME + " TEXT NOT NULL," +
+            LawyerContract.Case.CLIENT_ID + " INTEGER NOT NULL," +
+            LawyerContract.Case.AGAINST + " TEXT NOT NULL," +
+            LawyerContract.Case.SITUATION + " TEXT," +
+            LawyerContract.Case.CASE_DATE + " TEXT NOT NULL," +
+            LawyerContract.Case.COURT_LOCATION + " TEXT NOT NULL," +
+            LawyerContract.Case.DESCRIPTION + " TEXT," +
+            LawyerContract.Case.STATUS + " INTEGER NOT NULL DEFAULT 0," +
+            LawyerContract.Case.KEY_POINTS + " TEXT,FOREIGN KEY(" +
+            LawyerContract.Case.CLIENT_ID + ") REFERENCES " +
+            LawyerContract.Client.TABLE_NAME + "(" + LawyerContract.Client._ID + "));";
+
+    private String CREATE_DOCUMENT_TABLE = "CREATE TABLE " + LawyerContract.Document.TABLE_NAME + "(" +
+            LawyerContract.Document._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            LawyerContract.Document.DOCUMENT_NAME + " TEXT NOT NULL," +
+            LawyerContract.Document.FILE_PATH + " TEXT NOT NULL UNIQUE," +
+            LawyerContract.Document.CASE_ID + " INTEGER NOT NULL," +
+            LawyerContract.Document.LAST_UPDATED_DATE + " TEXT NOT NULL," +
+            "FOREIGN KEY(" + LawyerContract.Document.CASE_ID + ") REFERENCES " +
+            LawyerContract.Case.TABLE_NAME + "(" + LawyerContract.Case._ID + "));";
+
+    private String CREATE_REMINDER_TABLE = "CREATE TABLE " + LawyerContract.Reminder.TABLE_NAME + " (" +
+            LawyerContract.Reminder._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            LawyerContract.Reminder.REMINDER_NAME + " TEXT NOT NULL," +
+            LawyerContract.Reminder.START_DATE_TIME + " TEXT NOT NULL," +
+            LawyerContract.Reminder.END_DATE_TIME + " TEXT NOT NULL," +
+            LawyerContract.Reminder.LOCATION + " TEXT NOT NULL," +
+            LawyerContract.Reminder.NOTE + " TEXT," +
+            LawyerContract.Reminder.COLOUR + " TEXT NOT NULL," +
+            LawyerContract.Reminder.CASE_ID + " INTEGER NOT NULL," +
+            "FOREIGN KEY(" + LawyerContract.Reminder.CASE_ID + ") REFERENCES " +
+            LawyerContract.Case.TABLE_NAME + "(" + LawyerContract.Case._ID + "));";
+
+    public DbRepository(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_CLIENT_TABLE);
+            Log.d(TAG, "##Client Table Create " + CREATE_CLIENT_TABLE);
+        } catch (SQLiteException e) {
+            Log.e(TAG, "##Could not create client table" + e.toString());
+        }
+        try {
+            db.execSQL(CREATE_CASE_TABLE);
+            Log.d(TAG, "##Case Table Create " + CREATE_CASE_TABLE);
+        } catch (SQLiteException e) {
+            Log.e(TAG, "##Could not create case table" + e.toString());
+        }
+        try {
+            db.execSQL(CREATE_DOCUMENT_TABLE);
+            Log.d(TAG, "##Document Table Create " + CREATE_DOCUMENT_TABLE);
+        } catch (SQLiteException e) {
+            Log.e(TAG, "##Could not create document table" + e.toString());
+        }
+        try {
+            db.execSQL(CREATE_REMINDER_TABLE);
+            Log.d(TAG, "##Reminder Table Create " + CREATE_REMINDER_TABLE);
+        } catch (SQLiteException e) {
+            Log.e(TAG, "##Could not create reminder table" + e.toString());
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/database/LawyerContract.java
@@ -0,0 +1,102 @@
+package com.vibeosys.lawyerdiary.database;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by akshay on 09-09-2016.
+ */
+public class LawyerContract {
+
+    public static final String CONTENT_AUTHORITY = "com.vibeosys.lawyerdiary.app";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_CLIENT = "client";
+    public static final String PATH_CASE = "case";
+    public static final String PATH_DOCUMENT = "document";
+    public static final String PATH_REMINDER = "reminder";
+
+
+    public static final class Client implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CLIENT).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_CLIENT;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_CLIENT;
+        public final static String TABLE_NAME = "client";
+        public final static String NAME = "name";
+        public final static String EMAIL = "email";
+        public final static String PH_NUMBER = "phone_number";
+        public final static String ADDRESS = "address";
+
+        public static Uri buildClientUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class Case implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_CASE).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_CASE;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_CASE;
+
+        public final static String TABLE_NAME = "case";
+        public final static String CASE_NAME = "case_name";
+        public final static String CLIENT_ID = "client_id";
+        public final static String AGAINST = "against";
+        public final static String SITUATION = "situation";
+        public final static String CASE_DATE = "case_date";
+        public final static String COURT_LOCATION = "court_location";
+        public final static String DESCRIPTION = "description";
+        public final static String STATUS = "status";
+        public final static String KEY_POINTS = "key_points";
+
+        public static final Uri buildCaseUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class Document implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_DOCUMENT).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_DOCUMENT;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_DOCUMENT;
+        public final static String TABLE_NAME = "document";
+        public final static String DOCUMENT_NAME = "name";
+        public final static String FILE_PATH = "file_path";
+        public final static String CASE_ID = "case_id";
+        public final static String LAST_UPDATED_DATE = "last_updated_date";
+
+        public static final Uri buildDocumentUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+    public static final class Reminder implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_REMINDER).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/"
+                + CONTENT_AUTHORITY + "/" + PATH_REMINDER;
+        public final static String TABLE_NAME = "reminder";
+        public final static String REMINDER_NAME = "name";
+        public final static String START_DATE_TIME = "start_date_time";
+        public final static String END_DATE_TIME = "end_date_time";
+        public final static String LOCATION = "location";
+        public final static String NOTE = "note";
+        public final static String COLOUR = "colour";
+        public final static String CASE_ID = "case_id";
+
+        public static final Uri buildReminderUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+}
