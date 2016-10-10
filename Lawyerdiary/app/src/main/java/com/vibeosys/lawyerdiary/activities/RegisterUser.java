b60bb152a8c503e36f2ce32615848b28c5a8c298
ShrinivasSH <shrinivas@vibeosys.com>
2016-10-10 18:58:56 +0530

    User Registration is done
    User Registration is done

--- Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/activities/RegisterUser.java
+++ Lawyerdiary/app/src/main/java/com/vibeosys/lawyerdiary/activities/RegisterUser.java
@@ -1,8 +1,14 @@
 package com.vibeosys.lawyerdiary.activities;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteException;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.Gravity;
 import android.view.View;
@@ -11,19 +17,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.vibeosys.lawyerdiary.R;
+import com.vibeosys.lawyerdiary.database.LawyerContract;
 
 public class RegisterUser extends BaseActivity implements View.OnClickListener {
 
-    private EditText mUserName ,mUserEmailId,mUserPassword,mConfirmPassword;
+    private EditText mUserName, mUserEmailId, mUserPassword, mConfirmPassword;
     private Button mUserRegisterBtn;
+    long userRegisterId;
+    private static final String TAG = RegisterUser.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
         mUserName = (EditText) findViewById(R.id.userName);
-        mUserEmailId =(EditText) findViewById(R.id.userEmail);
-        mUserPassword =(EditText) findViewById(R.id.UserPassword);
-        mConfirmPassword =(EditText) findViewById(R.id.UserConfPassword);
+        mUserEmailId = (EditText) findViewById(R.id.userEmail);
+        mUserPassword = (EditText) findViewById(R.id.UserPassword);
+        mConfirmPassword = (EditText) findViewById(R.id.UserConfPassword);
         mUserRegisterBtn = (Button) findViewById(R.id.registerUser);
         mUserRegisterBtn.setOnClickListener(RegisterUser.this);
     }
@@ -31,63 +41,87 @@ public class RegisterUser extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        switch (id)
-        {
+        switch (id) {
             case R.id.registerUser:
-              boolean returnVal  = callToValidation();
-                if(returnVal==true)
-                {
-                    Toast toast =Toast.makeText(getApplicationContext(),"All validations are done",Toast.LENGTH_LONG);
-                    toast.setGravity(Gravity.CENTER,0,0);
-                    toast.show();
+                boolean returnVal = callToValidation();
+                if (returnVal == true) {
+                    boolean databaseVal = callToInsertValue();
+                    if(databaseVal ==true)
+                    {
+                        Intent loginScreen = new Intent(RegisterUser.this,LoginActivity.class);
+                        startActivity(loginScreen);
+                        finish();
+                        Toast toast = Toast.makeText(getApplicationContext(), "User Register Successfully", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                    }
+                    else if(databaseVal ==false)
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(), "User cannot register.Some thing went wrong", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                    }
                 }
                 break;
         }
     }
-    private boolean callToValidation()
-    {
-        String userName= mUserName.getText().toString().trim();
+
+    private boolean callToValidation() {
+        String userName = mUserName.getText().toString().trim();
         String userEmail = mUserEmailId.getText().toString().trim();
         String userPassword = mUserPassword.getText().toString().trim();
-        String userConfirmPwd =mConfirmPassword.getText().toString().trim();
-        if(mUserName.getText().toString().trim().length()==0)
-        {
+        String userConfirmPwd = mConfirmPassword.getText().toString().trim();
+        if (mUserName.getText().toString().trim().length() == 0) {
             mUserName.requestFocus();
             mUserName.setError(getResources().getString(R.string.str_user_name_val));
             return false;
-        }else if(mUserEmailId.getText().toString().trim().length()==0)
-        {
+        } else if (mUserEmailId.getText().toString().trim().length() == 0) {
             mUserEmailId.requestFocus();
             mUserEmailId.setError(getResources().getString(R.string.str_email_validation));
             return false;
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches())
-        {
+        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
             mUserEmailId.requestFocus();
             mUserEmailId.setError(getResources().getString(R.string.str_email_pattern_validation));
             return false;
         }
-        if(mUserPassword.getText().toString().trim().length()==0)
-        {
+        if (mUserPassword.getText().toString().trim().length() == 0) {
             mUserPassword.requestFocus();
             mUserPassword.setError(getResources().getString(R.string.str_pwd_validation));
             return false;
-        }else if(mConfirmPassword.getText().toString().trim().length()==0)
-        {
+        } else if (mConfirmPassword.getText().toString().trim().length() == 0) {
             mConfirmPassword.requestFocus();
             mConfirmPassword.setError(getResources().getString(R.string.str_conf_pwd_validation));
             return false;
         }
-        if(!TextUtils.isEmpty(userPassword)&& !TextUtils.isEmpty(userConfirmPwd))
-        {
-            if(!userPassword.equals(userConfirmPwd))
-            {
+        if (!TextUtils.isEmpty(userPassword) && !TextUtils.isEmpty(userConfirmPwd)) {
+            if (!userPassword.equals(userConfirmPwd)) {
                 mConfirmPassword.requestFocus();
                 mConfirmPassword.setError(getResources().getString(R.string.str_conf_pwd));
                 return false;
             }
         }
 
+        return true;
+    }
+
+    private boolean callToInsertValue() {
+        String userName = mUserName.getText().toString().trim();
+        String userEmail = mUserEmailId.getText().toString().trim();
+        String userPassword = mUserPassword.getText().toString().trim();
+        String userConfirmPwd = mConfirmPassword.getText().toString().trim();
+        ContentValues registerUser = new ContentValues();
+        registerUser.put(LawyerContract.UserLogin.USER_NAME, userName);
+        registerUser.put(LawyerContract.UserLogin.USER_EMAIL_ID, userEmail);
+        registerUser.put(LawyerContract.UserLogin.USER_PASSWORD, userPassword);
+        try {
+
+            Uri insertUserLogin = getContentResolver().insert(LawyerContract.UserLogin.CONTENT_URI, registerUser);
+            userRegisterId = ContentUris.parseId(insertUserLogin);
+        } catch (SQLiteException e) {
+            Log.e(TAG, "Login data is not inserted" + e.toString());
+            return false;
+        }
         return true;
     }
 }
