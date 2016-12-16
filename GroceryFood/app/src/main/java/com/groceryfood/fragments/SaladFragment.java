9025e9f6d2b3e040e8566f5961edfa61b01c30f1
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-16 13:20:47 +0530

    UI Changes in second page
    UI Changes in second page

--- GroceryFood/app/src/main/java/com/groceryfood/fragments/SaladFragment.java
+++ GroceryFood/app/src/main/java/com/groceryfood/fragments/SaladFragment.java
@@ -1,9 +1,24 @@
 package com.groceryfood.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.groceryfood.R;
 
 /**
  * Created by akshay on 15-12-2016.
  */
 public class SaladFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view =inflater.inflate(R.layout.fragment_salad, container, false);
+        return view;
+
+    }
 }
