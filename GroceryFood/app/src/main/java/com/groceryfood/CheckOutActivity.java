bb6636ea4f38b17333887a045d1766c85bfa25e8
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-19 16:11:11 +0530

    UI changes in finish order screen
    UI changes in finish order screen

--- GroceryFood/app/src/main/java/com/groceryfood/CheckOutActivity.java
+++ GroceryFood/app/src/main/java/com/groceryfood/CheckOutActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class CheckOutActivity extends AppCompatActivity {
 
-    TextView addMore;
+    TextView addMore, checkOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,19 @@ public class CheckOutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_check_out);
         setTitle("");
         addMore = (TextView) findViewById(R.id.txtAddMore);
+        checkOut = (TextView) findViewById(R.id.checkOut);
         addMore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
+            }
+        });
+        checkOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OrederFinishActivity.class);
+                startActivity(intent);
             }
         });
     }
