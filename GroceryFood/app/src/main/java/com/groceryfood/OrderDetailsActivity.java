6038594280522506f2dc83db327c83072c6ec5a7
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-21 20:59:46 +0530

    Back button is added and Title is set
    Back button is added and Title is set

--- GroceryFood/app/src/main/java/com/groceryfood/OrderDetailsActivity.java
+++ GroceryFood/app/src/main/java/com/groceryfood/OrderDetailsActivity.java
@@ -9,5 +9,6 @@ public class OrderDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+        setTitle("ORDER DETAILS");
     }
 }
