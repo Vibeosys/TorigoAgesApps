e00a3f87c9ba32b50f32cf6c898f4832ce26979d
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-21 18:45:44 +0530

    order details in tracking page is added
    order details in tracking page is added

--- GroceryFood/app/src/main/java/com/groceryfood/DisplayTrackOrdersActivity.java
+++ GroceryFood/app/src/main/java/com/groceryfood/DisplayTrackOrdersActivity.java
@@ -1,14 +1,26 @@
 package com.groceryfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DisplayTrackOrdersActivity extends AppCompatActivity {
+    TextView mOrderDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_track_orders);
-        setTitle("TRACK ORDERS");
+        setTitle(getString(R.string.str_track_order_title));
+        mOrderDetails = (TextView) findViewById(R.id.orderDetails);
+        mOrderDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OrderDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
