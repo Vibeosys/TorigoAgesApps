261af4981806688ebd4000d19ed1a1d383343a96
ShrinivasSH <shrinivas@vibeosys.com>
2016-12-24 15:37:33 +0530

    Login user page Changes
    Login user page Changes

--- GroceryFood/app/src/main/java/com/groceryfood/UserLoginActivity.java
+++ GroceryFood/app/src/main/java/com/groceryfood/UserLoginActivity.java
@@ -1,13 +1,16 @@
 package com.groceryfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 public class UserLoginActivity extends AppCompatActivity {
-TextView mForgotPwd;
+    TextView mForgotPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /** Making this activity, full screen */
@@ -17,6 +20,13 @@ TextView mForgotPwd;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
         mForgotPwd = (TextView) findViewById(R.id.forgotPw);
+        mForgotPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
