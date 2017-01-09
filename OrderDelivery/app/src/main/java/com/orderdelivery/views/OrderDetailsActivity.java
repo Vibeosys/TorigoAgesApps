d1f1e7f381c5a81ef70499e664aaa96cb468dc4c
ShrinivasSH <shrinivas@vibeosys.com>
2017-01-09 18:45:32 +0530

    UI changes in navigation drawer and Order details screen
    UI changes in navigation drawer and Order details screen

--- OrderDelivery/app/src/main/java/com/orderdelivery/views/OrderDetailsActivity.java
+++ OrderDelivery/app/src/main/java/com/orderdelivery/views/OrderDetailsActivity.java
@@ -11,5 +11,6 @@ public class OrderDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+        setTitle("Order details");
     }
 }
