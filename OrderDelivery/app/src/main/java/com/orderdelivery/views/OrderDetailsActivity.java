2b0f9909a16d7b821e1551d128b68199e96f382c
ShrinivasSH <shrinivas@vibeosys.com>
2017-01-09 19:43:26 +0530

    UI Changes
    UI Changes

--- OrderDelivery/app/src/main/java/com/orderdelivery/views/OrderDetailsActivity.java
+++ OrderDelivery/app/src/main/java/com/orderdelivery/views/OrderDetailsActivity.java
@@ -1,16 +1,28 @@
 package com.orderdelivery.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.orderdelivery.R;
 
 public class OrderDetailsActivity extends AppCompatActivity {
+    private Button mStartTrip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
         setTitle("Order details");
+        mStartTrip = (Button) findViewById(R.id.start_trip);
+        mStartTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ViewTripDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
