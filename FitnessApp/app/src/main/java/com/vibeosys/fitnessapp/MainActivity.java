e26a0d1cdb4fdcd5ecafca9a407fd4a4e950fbc8
ShrinivasSH <shrinivas@vibeosys.com>
2017-03-13 18:56:42 +0530

    Login Functionality is done
    Login Functionality is done

--- FitnessApp/app/src/main/java/com/vibeosys/fitnessapp/MainActivity.java
+++ FitnessApp/app/src/main/java/com/vibeosys/fitnessapp/MainActivity.java
@@ -2,15 +2,21 @@ package com.vibeosys.fitnessapp;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vibeosys.fitnessapp.activities.BaseActivity;
 import com.vibeosys.fitnessapp.activities.DashboardActivity;
@@ -25,6 +31,8 @@ public class MainActivity extends BaseActivity {
     private Button login;
     private ImageView circularImageView;
     private TextView mNewUser;
+    private EditText mUserEmailId, mUserPassword;
+    private String[] mSelectionArgs = {""};
 
 
     @Override
@@ -38,11 +46,36 @@ public class MainActivity extends BaseActivity {
         Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
         circularImageView.setImageBitmap(circularBitmap);
         mNewUser = (TextView) findViewById(R.id.newUser);
+        mUserEmailId = (EditText) findViewById(R.id.editTextusername);
+        mUserPassword = (EditText) findViewById(R.id.editTextuserpassword);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent nextAction = new Intent(getApplicationContext(), DashboardActivity.class);
-                startActivity(nextAction);
+                /*Intent nextAction = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(nextAction);*/
+                boolean val = validation();
+                if (val) {
+                    String selectionArg = FitnessContract.UserLogin.USER_EMAIL_ID + " and " + FitnessContract.UserLogin.USER_PASSWORD;
+                    String[] args = {mUserEmailId.getText().toString().trim(), mUserPassword.getText().toString().trim()};
+                    try {
+                        Cursor cursor = getContentResolver().query(FitnessContract.UserLogin.CONTENT_URI, null, "user_email = ? AND user_password = ?", args, null);
+                        Log.e(TAG, cursor.toString());
+                        int jk = cursor.getCount();
+                        if (cursor == null) {
+                            Log.e(TAG, cursor.toString());
+                        } else if (cursor.getCount() < 1) {
+                            Log.e(TAG, cursor.toString());
+                        }
+                        if (cursor.getCount() >= 1) {
+                            Toast.makeText(getApplicationContext(), "Valid user", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (SQLException e) {
+                        Log.e(TAG, e.toString());
+                    } catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+
+                }
             }
         });
 
@@ -53,6 +86,29 @@ public class MainActivity extends BaseActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean validation() {
+        String UserEmailId = mUserEmailId.getText().toString().trim();
+        String UserPassword = mUserPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(UserEmailId)) {
+            mUserEmailId.requestFocus();
+            mUserEmailId.setError("Please enter emailid");
+            return false;
+        } else if (!TextUtils.isEmpty(UserEmailId)) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(mUserEmailId.getText().toString()).matches()) {
+                mUserEmailId.requestFocus();
+                mUserEmailId.setError("Invalid emailid");
+                return false;
+            }
+
+        }
+        if (TextUtils.isEmpty(UserPassword)) {
+            mUserPassword.requestFocus();
+            mUserPassword.setError("Please enter password");
+            return false;
+        }
+        return true;
     }
 }
 
