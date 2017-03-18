b7ef9dec1f3b8737e01cf54c22c96f96cb00dadd
ShrinivasSH <shrinivas@vibeosys.com>
2017-03-18 12:23:34 +0530

    Diet plan changes
    Diet plan changes

--- FitnessApp/app/src/main/java/com/vibeosys/fitnessapp/activities/DietPlanActivity.java
+++ FitnessApp/app/src/main/java/com/vibeosys/fitnessapp/activities/DietPlanActivity.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.vibeosys.fitnessapp.R;
@@ -17,12 +19,16 @@ import com.vibeosys.fitnessapp.database.FitnessContract;
 public class DietPlanActivity extends AppCompatActivity {
     private Context context;
     private static final String TAG = DietPlanActivity.class.getSimpleName();
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.diet_plan);
+        //   setContentView(R.layout.diet_plan);
+        setContentView(R.layout.diet_plan_web_view);
         getSupportActionBar().setTitle("Diet plan");
+        webView = (WebView) findViewById(R.id.webView_diet);
+        // webView.loadUrl("file:///android_res/raw/normal.html");
         context = DietPlanActivity.this;
         String args[] = {"22", "22"};
         try {
@@ -34,11 +40,12 @@ public class DietPlanActivity extends AppCompatActivity {
                 Log.e(TAG, cursor.toString());
             }
             if (cursor.getCount() >= 1) {
-                Toast.makeText(getApplicationContext(), "Valid user", Toast.LENGTH_SHORT).show();
-                if (cursor.moveToFirst()){
+                if (cursor.moveToFirst()) {
                     String userEmailId = cursor.getString(cursor.getColumnIndex(FitnessContract.UserDiet.USER_DIET));
-                    Log.e(TAG, TAG);
-                    Log.e(TAG, TAG);
+                    String url = "file:///android_res/raw/" + userEmailId;
+
+                    webView.loadUrl(url);
+
                 }
 
             }
