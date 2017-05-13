ef1c48608374af087beabe601c64bd49246d7a90
ShrinivasSH <shrinivas@vibeosys.com>
2017-05-13 12:13:01 +0530

    Consultation second screen is done
    Consultation second screen is done

--- FinaQA/app/src/main/java/com/finaqa/activities/ConsultantRegistration.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/ConsultantRegistration.java
@@ -1,18 +1,36 @@
 package com.finaqa.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.finaqa.R;
 
-public class ConsultantRegistration extends AppCompatActivity {
+public class ConsultantRegistration extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mNextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultant_registration);
+        mNextButton = (Button) findViewById(R.id.consult_first);
+        mNextButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.consult_first:
+                Intent intent = new Intent(getApplicationContext(), ConsultantSecondRegistration.class);
+                startActivity(intent);
+                break;
+        }
 
     }
 }
