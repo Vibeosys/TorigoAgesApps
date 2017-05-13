7d73deab1ec85f6e32d92ef93d50b3dbb8a4cbc4
ShrinivasSH <shrinivas@vibeosys.com>
2017-05-13 12:42:05 +0530

    Consultant registration is done
    Consultant registration is done

--- FinaQA/app/src/main/java/com/finaqa/activities/ConsultantSecondRegistration.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/ConsultantSecondRegistration.java
@@ -1,15 +1,34 @@
 package com.finaqa.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.finaqa.R;
 
-public class ConsultantSecondRegistration extends AppCompatActivity {
+public class ConsultantSecondRegistration extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mNextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultant_second_registration);
+        mNextBtn = (Button) findViewById(R.id.consult_second);
+        mNextBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.consult_second:
+                Intent intent = new Intent(getApplicationContext(), ConsultantThirdRegistration.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
---
+++ FinaQA/app/src/main/java/com/finaqa/activities/ConsultantThirdRegistration.java
@@ -0,0 +1,15 @@
+package com.finaqa.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.finaqa.R;
+
+public class ConsultantThirdRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultant_third_registration);
+    }
+}
