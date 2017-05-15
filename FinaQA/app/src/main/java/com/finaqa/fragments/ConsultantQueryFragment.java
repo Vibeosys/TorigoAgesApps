0e1fa1ec952bec352f962b1c868f2b8ac3f41e5b
ShrinivasSH <shrinivas@vibeosys.com>
2017-05-15 12:05:28 +0530

    UI changes
    UI changes

--- FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantQueryFragment.java
+++ FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantQueryFragment.java
@@ -1,11 +1,13 @@
 package com.finaqa.fragments;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.finaqa.R;
 
@@ -14,6 +16,7 @@ import com.finaqa.R;
  */
 public class ConsultantQueryFragment extends Fragment {
 
+    private TextView more1, more2, more3;
 
     public ConsultantQueryFragment() {
         // Required empty public constructor
@@ -23,8 +26,36 @@ public class ConsultantQueryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_consultant_query, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consultant_query, container, false);
+        more1 = (TextView) view.findViewById(R.id.more);
+        more2 = (TextView) view.findViewById(R.id.more1);
+        more3 = (TextView) view.findViewById(R.id.feedBack1);
+        more1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        more2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        more3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+        return view;
     }
 
+    private void openDialog() {
+        final Dialog dialog = new Dialog(getContext(), R.style.CustomDialog); // Context, this, etc.
+        dialog.setContentView(R.layout.feedback_dialog);
+        dialog.setTitle(R.string.dialog_title);
+        dialog.show();
+    }
 }
