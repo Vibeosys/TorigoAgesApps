dafdb2bf13939604f3c8ada6988ba2a21dc33ad4
ShrinivasSH <shrinivas@vibeosys.com>
2017-05-19 12:19:16 +0530

    About us and contact us page is added
    About us and contact us page is added

---
+++ FinaQA/app/src/main/java/com/finaqa/activities/ContactUsActivity.java
@@ -0,0 +1,21 @@
+package com.finaqa.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.finaqa.R;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+
+    }
+
+}
--- FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantMoreFragment.java
+++ FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantMoreFragment.java
@@ -1,28 +1,52 @@
 package com.finaqa.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.finaqa.R;
+import com.finaqa.activities.AboutUsActivity;
+import com.finaqa.activities.ContactUsActivity;
 
 
-public class ConsultantMoreFragment extends Fragment {
+public class ConsultantMoreFragment extends Fragment implements View.OnClickListener {
 
-
+    private LinearLayout mContactUs, mAboutUs;
 
     public ConsultantMoreFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_consultant_more, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consultant_more, container, false);
+        mContactUs = (LinearLayout) view.findViewById(R.id.layout_contactUs);
+        mAboutUs = (LinearLayout) view.findViewById(R.id.about_us);
+        mContactUs.setOnClickListener(this);
+        mAboutUs.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.layout_contactUs:
+                Intent intent = new Intent(getContext().getApplicationContext(), ContactUsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.about_us:
+                Intent intent1 = new Intent(getContext().getApplicationContext(), AboutUsActivity.class);
+                startActivity(intent1);
+                break;
+        }
+    }
 }
