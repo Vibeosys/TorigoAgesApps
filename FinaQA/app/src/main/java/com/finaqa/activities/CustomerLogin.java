e736bfb76640d065fde25df7465de150f446ad93
ShrinivasSH <shrinivas@vibeosys.com>
2017-06-07 15:50:41 +0530

    UI changes in chat fragment
    UI changes in chat fragment

--- FinaQA/app/src/main/java/com/finaqa/activities/CustomerLogin.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/CustomerLogin.java
@@ -4,20 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.finaqa.MainActivity;
 import com.finaqa.R;
 
 public class CustomerLogin extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout mNewUserLayout;
+    private Button mUserLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
         mNewUserLayout = (LinearLayout) findViewById(R.id.newUser);
+        mUserLogin = (Button) findViewById(R.id.userLoginBtn);
         setTitle("User Login");
         mNewUserLayout.setOnClickListener(this);
+        mUserLogin.setOnClickListener(this);
     }
 
     @Override
@@ -27,6 +32,11 @@ public class CustomerLogin extends AppCompatActivity implements View.OnClickList
             case R.id.newUser:
                 Intent intent = new Intent(getApplicationContext(), RegisterUserActivity.class);
                 startActivity(intent);
+                break;
+            case R.id.userLoginBtn:
+                Intent intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent1);
+                break;
         }
 
     }
--- FinaQA/app/src/main/java/com/finaqa/fragments/FragmentAnswer.java
+++ FinaQA/app/src/main/java/com/finaqa/fragments/FragmentAnswer.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.finaqa.MainActivity;
@@ -18,6 +19,8 @@ import com.finaqa.R;
 public class FragmentAnswer extends Fragment {
 
     private TextView mFeedBack;
+    private LinearLayout mReplyLayout;
+    private String CHAT_FRAGMENT = "chat";
 
     public FragmentAnswer() {
         // Required empty public constructor
@@ -28,12 +31,21 @@ public class FragmentAnswer extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment_answer, container, false);
+        mReplyLayout = (LinearLayout) view.findViewById(R.id.replyTv);
         // Inflate the layout for this fragment
-        mFeedBack = (TextView) view.findViewById(R.id.feedBack);
+       /* mFeedBack = (TextView) view.findViewById(R.id.feedBack);
         mFeedBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openDialog();
+            }
+        });*/
+        mReplyLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment_chat fragment_chat = new Fragment_chat();
+                getFragmentManager().beginTransaction().
+                        replace(R.id.fragment_frame_lay, fragment_chat, CHAT_FRAGMENT).commit();
             }
         });
         return view;
