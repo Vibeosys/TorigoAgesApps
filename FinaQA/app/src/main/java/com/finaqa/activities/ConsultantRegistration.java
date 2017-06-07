09d44624be99c54c6611fa03666d67423fd01414
ShrinivasSH <shrinivas@vibeosys.com>
2017-06-07 18:54:06 +0530

    UI changes in user and consultant registration page
    UI changes in user and consultant registration page

--- FinaQA/app/src/main/java/com/finaqa/activities/ConsultantRegistration.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/ConsultantRegistration.java
@@ -1,31 +1,40 @@
 package com.finaqa.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.finaqa.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 public class ConsultantRegistration extends AppCompatActivity implements View.OnClickListener {
 
     private Button mNextButton;
+    private EditText mDateOfBirth;
+    private Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultant_registration);
         mNextButton = (Button) findViewById(R.id.consult_first);
-
+        mDateOfBirth = (EditText) findViewById(R.id.dateOfBirth);
+        myCalendar = Calendar.getInstance();
         mNextButton.setOnClickListener(this);
-
+        mDateOfBirth.setOnClickListener(this);
 
     }
 
@@ -37,7 +46,34 @@ public class ConsultantRegistration extends AppCompatActivity implements View.On
                 Intent intent = new Intent(getApplicationContext(), ConsultantSecondRegistration.class);
                 startActivity(intent);
                 break;
+            case R.id.dateOfBirth:
+                new DatePickerDialog(ConsultantRegistration.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                break;
         }
 
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mDateOfBirth.setText(sdf.format(myCalendar.getTime()));
     }
 }
--- FinaQA/app/src/main/java/com/finaqa/activities/RegisterUserActivity.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/RegisterUserActivity.java
@@ -1,15 +1,62 @@
 package com.finaqa.activities;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.finaqa.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class RegisterUserActivity extends AppCompatActivity {
+
+    private EditText mAge;
+    private Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        mAge = (EditText) findViewById(R.id.age);
+        myCalendar = Calendar.getInstance();
+
+        mAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(RegisterUserActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mAge.setText(sdf.format(myCalendar.getTime()));
     }
 }
