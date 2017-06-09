950cdd01c4efc402c5a37f900ca22335fe9c1886
ShrinivasSH <shrinivas@vibeosys.com>
2017-06-09 19:01:53 +0530

    new icons are added
    new icons are added

--- FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantPaymentFragment.java
+++ FinaQA/app/src/main/java/com/finaqa/fragments/ConsultantPaymentFragment.java
@@ -1,19 +1,23 @@
 package com.finaqa.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.finaqa.R;
+import com.finaqa.activities.WithdrawAmount;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConsultantPaymentFragment extends Fragment {
 
+    private TextView mWithdraw;
 
     public ConsultantPaymentFragment() {
         // Required empty public constructor
@@ -23,8 +27,17 @@ public class ConsultantPaymentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_consultant_payment, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consultant_payment, container, false);
+        mWithdraw = (TextView) view.findViewById(R.id.withdraw);
+        mWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), WithdrawAmount.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
