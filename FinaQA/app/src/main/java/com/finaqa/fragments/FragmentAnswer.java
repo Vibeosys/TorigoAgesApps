d431219924f6f48cb72d1e9123bac5a68f84d29b
ShrinivasSH <shrinivas@vibeosys.com>
2017-06-09 13:49:28 +0530

    UI changes in main activity
    UI changes in main activity

--- FinaQA/app/src/main/java/com/finaqa/fragments/FragmentAnswer.java
+++ FinaQA/app/src/main/java/com/finaqa/fragments/FragmentAnswer.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,6 +22,7 @@ public class FragmentAnswer extends Fragment {
     private TextView mFeedBack, mReadMore;
     private LinearLayout mReplyLayout;
     private String CHAT_FRAGMENT = "chat";
+    private Button acceptRequestBtn;
 
     public FragmentAnswer() {
         // Required empty public constructor
@@ -33,6 +35,7 @@ public class FragmentAnswer extends Fragment {
         View view = inflater.inflate(R.layout.fragment_fragment_answer, container, false);
         mReplyLayout = (LinearLayout) view.findViewById(R.id.replyTv);
         mReadMore = (TextView) view.findViewById(R.id.readMore);
+        acceptRequestBtn = (Button) view.findViewById(R.id.acceptRequest);
         // Inflate the layout for this fragment
        /* mFeedBack = (TextView) view.findViewById(R.id.feedBack);
         mFeedBack.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +58,14 @@ public class FragmentAnswer extends Fragment {
                 Fragment_chat fragment_chat = new Fragment_chat();
                 getFragmentManager().beginTransaction().
                         replace(R.id.fragment_frame_lay, fragment_chat, CHAT_FRAGMENT).commit();
+            }
+        });
+        acceptRequestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentPayment fragmentPayment = new FragmentPayment();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_frame_lay, fragmentPayment, CHAT_FRAGMENT).commit();
+
             }
         });
         return view;
