199310aa0c588d2ae6d22be5ca5aa5e6e3887bc8
ShrinivasSH <shrinivas@vibeosys.com>
2017-06-12 15:35:21 +0530

    Tooltip is added
    Tooltip is added

--- FinaQA/app/src/main/java/com/finaqa/activities/FinancialAssetActivity.java
+++ FinaQA/app/src/main/java/com/finaqa/activities/FinancialAssetActivity.java
@@ -13,8 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.finaqa.R;
+
+import io.github.douglasjunior.androidSimpleTooltip.SimpleTooltip;
 
 
 public class FinancialAssetActivity extends AppCompatActivity {
@@ -22,6 +25,7 @@ public class FinancialAssetActivity extends AppCompatActivity {
     private Button mButton;
     private ImageView mLiquid;
     LinearLayout linearLayout;
+    TextView mTooltip, label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +34,19 @@ public class FinancialAssetActivity extends AppCompatActivity {
         mButton = (Button) findViewById(R.id.next);
         mLiquid = (ImageView) findViewById(R.id.liquidImageView);
         linearLayout = (LinearLayout) findViewById(R.id.tooltipView);
+        label = (TextView) findViewById(R.id.label123);
         mLiquid.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View view) {
-                                           final Dialog dialog = new Dialog(getApplicationContext(), R.style.CustomDialog); // Context, this, etc.
-                                           dialog.setContentView(R.layout.feedback_dialog);
-                                           dialog.setTitle(R.string.dialog_title);
-                                           dialog.show();
+                                           new SimpleTooltip.Builder(FinancialAssetActivity.this)
+                                                   .anchorView(mLiquid)
+                                                   .text("Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+                                                           " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam")
+                                                   .gravity(Gravity.END)
+                                                   .animated(true)
+                                                   .transparentOverlay(false)
+                                                   .build()
+                                                   .show();
                                        }
                                    }
         );
